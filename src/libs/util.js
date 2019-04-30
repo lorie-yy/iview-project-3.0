659cf66b228f68ef7ec9b11ebbd9c4e39ffd5500
Imagine-Lorie <313966183@qq.com>
2019-04-30 18:48:32 +0800

    网页title

--- src/libs/util.js
+++ src/libs/util.js
@@ -2,7 +2,7 @@ let util = {
 
 };
 util.title = function (title) {
-    title = title ? title + ' - Home' : 'iView project';
+    title = title ? title + ' - 想象力商城后台管理系统' : '想象力商城后台管理系统';
     window.document.title = title;
 };
 
